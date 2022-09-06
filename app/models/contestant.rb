f39510b6365cb6b6eac2bd8339698fc466475d4c
Astrid Hecht <silentaviary@gmail.com>
2022-09-06 15:42:20 -0600

    feat: US 2 complete and passing contestants#index tests

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,4 +2,8 @@ class Contestant < ApplicationRecord
   validates_presence_of :name, :age, :hometown, :years_of_experience
   has_many :contestant_projects
   has_many :projects, through: :contestant_projects
+
+  def show_proj
+    projects.where('contestant_projects.contestant_id = ?', self.id)
+  end
 end
